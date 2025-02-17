d14e8e4801dc9c768dca2dc78de55f4b25b2572b
paula <paula.bravoa99@gmail.com>
2025-02-17 22:10:43 +0100

    .

--- src/BasicLevel/exercice4.py
+++ src/BasicLevel/exercice4.py
@@ -1,6 +1,17 @@
 # 4. **Scambio variabili**  
 #    Leggi un file json e modificalo con l'input del utente
 ''' cose da fare in poche parole 
+
 1. Leggere il contenuto di un file JSON.
 2. Modificare il contenuto del JSON in base all'input dell'utente.
-3. Salvare di nuovo il file JSON con i dati modificati.'''
+3. Salvare di nuovo il file JSON con i dati modificati.
+
+'''
+#with open ("Data/file.json","r") as file: #il file non si trova nella stessa cartella, scrivo DATA/ per trovarlo.
+#    print(file.read()) #visualizzazione a video di cosa contiene il file json.
+file=open("Data/file.json","r")
+print(c=file.read())
+#print(c)
+file2=open("Data/file.json","r")
+file2.read()
+print(file.read(),file2.read())
