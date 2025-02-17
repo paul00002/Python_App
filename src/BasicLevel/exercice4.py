3afe22a8403716f696a9c3cbc704ab20bc73a9f8
paula <paula.bravoa99@gmail.com>
2025-02-17 21:15:50 +0100

    devo vedere file json

--- src/BasicLevel/exercice4.py
+++ src/BasicLevel/exercice4.py
@@ -1,2 +1,6 @@
 # 4. **Scambio variabili**  
 #    Leggi un file json e modificalo con l'input del utente
+''' cose da fare in poche parole 
+1. Leggere il contenuto di un file JSON.
+2. Modificare il contenuto del JSON in base all'input dell'utente.
+3. Salvare di nuovo il file JSON con i dati modificati.'''
