90809306a779cd84b114d37ec7d5d30a6858a951
paula <paula.bravoa99@gmail.com>
2025-02-17 18:49:38 +0100

    .

--- src/BasicLevel/exercice1.py
+++ src/BasicLevel/exercice1.py
@@ -1,20 +1,30 @@
 # 1. **Max tra due numeri**  
 #    Chiedi due numeri all'utente e stampa il maggiore usando istruzioni `if/else`.
 
-#n=input("Max tra due numeri")
-#print(n)
-x=int(input("inserirsci il primo numero:"))
-print(x,type(x))
-#print(x)
-y=int(input("inserisci il secondo numero:"))
-print(y,type(y))
+#x=int(input("inserirsci il primo numero:"))
+#print(x,type(x))
+#y=int(input("inserisci il secondo numero:"))
+#print(y,type(y))
 #so che tipo di variabili sono sia x e y in più le ho visualizzate a video. 
 
-if x and y ==int: 
-    if x>y:
-        print("il primo valore è maggiore del secondo"+str(x))
-    elif x<y :
-        print("il secondo valore è maggiore del primo"+str(y))
-    elif x==y:
-        print(" i due  numeri sono uguali")
-        
+#funzione + controllo degli errori 
+def chiedi_numero():
+    while True:
+        try:
+            numero = int(input("Inserisci un numero intero: "))
+            return numero
+        except ValueError:
+            print("Errore: devi inserire un numero intero!")
+
+# Chiedi i due numeri all'utente
+numero1 = chiedi_numero()
+numero2 = chiedi_numero()
+
+# Confronta i numeri e stampa il maggiore
+if numero1 > numero2:
+    print(f"Il numero maggiore è: {numero1}")
+elif numero2 > numero1:
+    print(f"Il numero maggiore è: {numero2}")
+else:
+    print("I due numeri sono uguali.")
+
